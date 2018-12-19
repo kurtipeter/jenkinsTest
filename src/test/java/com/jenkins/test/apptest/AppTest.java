97decbcbc0b43803688ef0d7e0b923e373de5139
Peter Kurti <Peter_Kurti@epam.com>
2018-12-19 13:25:52 +0100

    initialize everything

---
+++ src/test/java/com/jenkins/test/apptest/AppTest.java
@@ -0,0 +1,17 @@
+package com.jenkins.test.apptest;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class AppTest {
+    @Test
+    public void passingTest() {
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void failingTest() {
+        Assert.assertTrue(false);
+    }
+}
