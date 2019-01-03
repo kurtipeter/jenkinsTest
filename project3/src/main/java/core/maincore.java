be199a6a2488376f8d2f799d6b8115b0a2ade8b5
Peter Kurti <Peter_Kurti@epam.com>
2019-01-03 17:08:05 +0100

    add project3

---
+++ project3/src/main/java/core/maincore.java
@@ -0,0 +1,9 @@
+package core;
+
+public class maincore {
+
+    public static void main(String[] args) {
+        System.out.println("This is project33333333333333333333333333333333333");
+    }
+
+}
